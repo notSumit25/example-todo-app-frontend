419407e0231b8a45a45e32384868252087066506
Sumit <sumitgarg380@gmail.com>
2024-05-05 19:12:14 +0530

    compo

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+    <div>
+      <input type="text" placeholder='title' />
+      <input type="text" placeholder='description' />
+        <button>Add Todo</button>
+    </div>
+  )
+}
+
+export default CreateTodo
